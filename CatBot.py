6fcab738efdda66da15923d629ec3431595ccd50
Dee419 <yorick.allie@ucll.student.be>
2022-08-06 22:42:35 +0200

    edit message log fix

--- CatBot.py
+++ CatBot.py
@@ -101,9 +101,6 @@ async def on_raw_message_edit(payload):
             if non_cached_message.id == payload.message_id:
                 message = non_cached_message
     if message is not None and chat_log_channel_id is not None and chat_log_channel_id != 0:
-        # Ignore messages where the content stays the same
-        if message.content == message.content:
-            return
         # We have the message in our cache and we have the chat log channel
         chat_log_channel = bot.get_channel(chat_log_channel_id)
         
@@ -111,6 +108,9 @@ async def on_raw_message_edit(payload):
         today = (datetime.date.today()).strftime("%d/%m/%Y")
         before = message
         after = await bot.get_channel(payload.channel_id).fetch_message(payload.message_id)
+        # Ignore messages where the content stays the same
+        if before.content == after.content:
+            return
         if before.content == "":
             before_content = "**No text to display**"
         else:
